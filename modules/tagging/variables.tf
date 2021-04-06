4c7bde5e81d5beaa1bf9a6ca9a1e7ffaf50a381d
Anil Kumar Nagaraj <anilkumar@in.ibm.com>
2021-04-06 20:27:47 +0530

    Add resource tag module

---
+++ modules/tagging/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_id" {
+  description = "CRN of the resource on which the tags should be attached"
+  type        = string
+  default     = ""
+}
+
+variable "resource_ids" {
+  description = "CRN of the resource on which the tags should be attached"
+  type        = list(string)
+  default     = []
+}
+
+variable "resource_type" {
+  description = "Resource type on which the tags should be attached"
+  type        = string
+  default     = ""
+}
+
+variable "tag_names" {
+  description = "Tags for the resource"
+  type        = list(string)
+
+  validation {
+      condition     = can([for s in var.tag_names : regex("^[a-zA-Z0-9:]+$", s)])
+      error_message = "Tags must be of the form `key:value`."
+  }
+}
+
